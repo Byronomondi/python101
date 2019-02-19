ead18c239437cb8a6f841a013d0c647fae231115
byronomondi <byron.lekas@gmail.com>
2019-02-19 18:26:49 +0300

    calc in python

---
+++ calc.py
@@ -0,0 +1,49 @@
+while True:
+  print("Options:")
+  print("Enter 'add' to  add two numbers")
+  print("Enter 'subtract' to subtract two numbers")
+  print("Enter 'multiply' to multiply two numbers")
+  print("Enter 'divide' to divide two numbers")
+  print("Enter 'quit' to end program")
+  user_input =input(":")
+
+if user_input=="quit":
+  break
+elif user_input=="add":
+  ...
+elif user_input=="subtract":
+  ...
+elif user_input=="multiply":
+  ...
+elif user_input=="divide":
+  ...
+else:
+  print("Unknown input")
+
+#Addition section
+elif user_input=="add":
+  num1 =float(input("Enter a number:"))
+  num2 =float(input("Enter another number:"))
+ 
+#Subtraction section
+elif user_input=="subtract":
+  num1 =float(input("Enter a number:"))
+  num2 =float(input("Enter another number:"))
+#process user info
+elif user_input=="add":
+  num1=float(input("Enter a number".))
+  num2=float(input("Enter another number."))
+  result=str(num1 + num2)
+  print("The answer is" + result)
+  
+
+#multiplication section
+elif user_input=="multiply":
+  num1 =float(input("Enter a number:"))
+  num2=float(input("Enter another number:"))
+
+#division section
+elif user_input=="divide":
+  num1=float(input("Enter a number:"))
+  num2=float(input("Enter another number:"))
+
